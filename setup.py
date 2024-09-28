5e5c86d0a3f1278a414c303d5e4e3fc888a19114
Sergio Morell-Monzó <59225676+sermomon@users.noreply.github.com>
2024-09-28 13:57:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyNAUTA",  # Asegúrate de que este nombre sea único en PyPI
-    version="0.1.0",  # Versión del paquete
-    description="Herramientas para trabajar con datos acústicos de sistemas subacuáticos.",
+    name="pyNAUTA",
+    version="0.1.0",
+    description="Software tools for data management and analysis of Acoustic Passive Monitoring Systems data captured by NAUTA scientific recorders.",
     author="Sergio Morell-Monzó",
     author_email="sermomon@upv.es",
-    url="https://github.com/sermomon/pyNAUTA",  # El repositorio donde está alojado tu código
-    packages=find_packages(),  # Encuentra automáticamente los paquetes dentro del directorio
-    include_package_data=True,  # Incluye archivos adicionales, como tu archivo WAV
+    url="https://github.com/sermomon/pyNAUTA",
+    packages=find_packages(),
+    include_package_data=True,
     package_data={
-        'NAUTA': ['data/example.wav'],  # Especifica los archivos de datos incluidos
+        'NAUTA': ['23d51_20240415_084603e.wav'],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
